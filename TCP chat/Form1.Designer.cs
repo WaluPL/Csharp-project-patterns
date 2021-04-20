f998fe3c9bed78f892854109064fd2ceb1772817
Michał Walkiewicz <52602306+WaluPL@users.noreply.github.com>
2021-04-20 20:20:20 +0200

    Add files via upload

---
+++ TCP chat/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace tcp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.chkServer = new System.Windows.Forms.CheckBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(105, 11);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 20);
+            this.txtHost.TabIndex = 0;
+            this.txtHost.Text = "localhost";
+            this.txtHost.TextChanged += new System.EventHandler(this.txtHost_TextChanged);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(225, 11);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(71, 20);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtPort.TextChanged += new System.EventHandler(this.txtPort_TextChanged);
+            // 
+            // chkServer
+            // 
+            this.chkServer.AutoSize = true;
+            this.chkServer.Location = new System.Drawing.Point(12, 13);
+            this.chkServer.Name = "chkServer";
+            this.chkServer.Size = new System.Drawing.Size(57, 17);
+            this.chkServer.TabIndex = 2;
+            this.chkServer.Text = "Server";
+            this.chkServer.UseVisualStyleBackColor = true;
+            this.chkServer.CheckedChanged += new System.EventHandler(this.chkServer_CheckedChanged);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 42);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(768, 360);
+            this.txtLog.TabIndex = 3;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 417);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(637, 20);
+            this.txtMessage.TabIndex = 4;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(667, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(667, 415);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.chkServer);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.CheckBox chkServer;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ TCP chat/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tcp
+{
+    public partial class Form1 : Form
+    {
+        private delegate void LogAddProc(string message);
+        private delegate void AdjustControlsProc();
+        private UInt16 port = 4201;
+        private bool IsConnected = false;
+        private TcpListener server;
+        private TcpClient client;
+        private List<Socket> clients = new List<Socket>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtPort.Text = port.ToString();
+            AdjustControls();
+        }
+
+        private void LogAdd(string message)
+        {
+            if(InvokeRequired)
+            {
+                Invoke(new LogAddProc(LogAdd),message);
+            }
+            else
+            {
+                txtLog.AppendText(message + Environment.NewLine);
+            }           
+        }
+
+        private void AdjustControls()
+        {
+            if (InvokeRequired) Invoke(new AdjustControlsProc(AdjustControls));
+            else
+            {
+                string message = txtMessage.Text.Trim();
+                bool isNotEmpty = message.Length > 0;
+                btnSend.Enabled = isNotEmpty;
+                if (IsConnected)
+                {
+                    chkServer.Enabled = false;
+                    txtHost.Enabled = false;
+                    txtPort.Enabled = false;
+                    btnConnect.Text = "Disconnect";
+                }
+                else
+                {
+                    chkServer.Enabled = true;
+                    txtHost.Enabled = !chkServer.Checked;
+                    txtPort.Enabled = true;
+                    btnConnect.Text = "Connect";
+                }
+            }
+        }
+
+        private void ServerThread()
+        {
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            LogAdd("Server conntected");
+            IsConnected = true;
+            AdjustControls();
+            while (IsConnected)
+            {
+                if(server.Pending())
+                {
+                    new Thread(ServerClientThread).Start();
+                }
+            }
+            IsConnected = false;
+            AdjustControls();
+            LogAdd("Server disconntected");
+            server.Stop();
+            
+        }
+
+        private void ServerClientThread()
+        {
+            Socket socket;
+            try
+            {
+                socket = server.AcceptSocket();
+            }
+            catch (SocketException)
+            {
+                return;
+            }
+            
+            string who = socket.RemoteEndPoint.ToString();
+            lock (clients) clients.Add(socket);            
+            LogAdd($"Client conntected { who } ");
+            while(IsConnected)
+            {
+                if (socket.Poll(50000, SelectMode.SelectRead))
+                {
+                    int size = socket.Available;
+                    if (size <= 0) break;
+                    byte[] buffor = new byte[size];
+                    socket.Receive(buffor);
+                    string message = who + ": " + UTF8Encoding.UTF8.GetString(buffor);
+                    buffor = UTF8Encoding.UTF8.GetBytes(message);
+                    lock (clients) foreach (Socket s in clients) s.Send(buffor);
+                    LogAdd(message);
+                }
+            }
+            LogAdd($"Client { who } disconntected");
+            lock (clients) clients.Remove(socket);
+            socket.Disconnect(false);
+        }
+
+        private void ClientThread()
+        {
+            try
+            {
+                client = new TcpClient(TextHost.Text.Trim(), port);
+            }
+            catch (SocketException)
+            {
+                AppendLog("Serwer offline");
+                return;
+            }
+            
+            Socket socket = client.Client;
+            LogAdd("Connected");
+            IsConnected = true;
+            AdjustControls();
+            while (IsConnected)
+            {
+                if (socket.Poll(50000, SelectMode.SelectRead))
+                {
+                    int size = socket.Available;
+                    if (size <= 0) break;
+                    byte[] buffor = new byte[size];
+                    socket.Receive(buffor);
+                    string message = UTF8Encoding.UTF8.GetString(buffor);
+                    LogAdd(message);
+                }
+            }
+			
+            IsConnected = false;
+            AdjustControls();
+            LogAdd("Disconnected");
+            socket.Disconnect(false);
+
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text.Trim();
+            byte[] buffor = UTF8Encoding.UTF8.GetBytes(message);
+            
+            if (chkServer.Checked)
+            
+                LogAdd(message);
+                lock (clients) foreach (Socket s in clients) s.Send(buffor);
+            }
+            else {
+                client.Client.Send(buffor);
+            }
+            
+
+        }
+
+        private void txtPort_TextChanged(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = UInt16.TryParse(txtPort.Text, out port ) && (txtHost.Text.Trim().Length > 0 || chkServer.Checked);
+            AdjustControls();
+        }
+
+        private void chkServer_CheckedChanged(object sender, EventArgs e)
+        {
+            txtHost.Enabled = !chkServer.Checked;
+            txtPort_TextChanged(sender, e);
+        }
+
+        private void txtHost_TextChanged(object sender, EventArgs e)
+        {
+            txtPort_TextChanged(sender, e);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (IsConnected)
+            {
+                IsConnected = false;
+            }
+            else
+            {
+                if (chkServer.Checked)
+                {
+                    new Thread(ServerThread).Start();
+                }
+                else
+                {
+                    new Thread(ClientThread).Start();
+                }
+            }
+            AdjustControls();
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            AdjustControls();
+        }
+    }
+}
